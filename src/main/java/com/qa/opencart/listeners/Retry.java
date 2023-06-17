16b122377d0c566304db4a82c7d62eb6aa5f9f69
Robinson Martinez Martinez <robinsonmartinez23@hotmail.com>
2023-06-17 16:19:48 -0400

    Reduced retry times

--- src/main/java/com/qa/opencart/listeners/Retry.java
+++ src/main/java/com/qa/opencart/listeners/Retry.java
@@ -5,12 +5,12 @@ import org.testng.ITestResult;
 
 public class Retry implements IRetryAnalyzer {
     private int count = 0;
-    private static int maxTry = 3;
+    private static int maxTry = 2;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
-        if (!iTestResult.isSuccess()) { // Check if test not succeed
-            if (count < maxTry) { // Check if maxtry count is reached
+        if (!iTestResult.isSuccess()) { // Check if test not succeeds
+            if (count < maxTry) { // Check if max tries count is reached
                 count++; // Increase the maxTry count by 1
                 iTestResult.setStatus(ITestResult.FAILURE); // Mark test as failed
                 return true; // Tells TestNG to re-run the test
@@ -18,7 +18,7 @@ public class Retry implements IRetryAnalyzer {
                 iTestResult.setStatus(ITestResult.FAILURE); // If maxCount reached,test marked as failed
             }
         } else {
-            iTestResult.setStatus(ITestResult.SUCCESS); // If test passes, TestNG marks it as passed
+            iTestResult.setStatus(ITestResult.SUCCESS); // If the test passes, TestNG marks it as passed
         }
         return false;
     }
